f0be5ae5e4403d12ae7d5f2b288b95de2ffd6dd7
Alex Carlton <ahcarlton@gmail.com>
2017-05-24 16:16:08 -0400

    Added Queries Project

---
+++ Queries/Movie.cs
@@ -0,0 +1,23 @@
+﻿namespace Queries
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public float Rating { get; set; }
+        
+
+        int _year;
+        public int Year
+        {
+            get
+            {
+                System.Console.WriteLine($"Returning {_year} for {Title}");
+                return _year;
+            }
+            set
+            {
+                _year = value;
+            }
+        }
+    }
+}
